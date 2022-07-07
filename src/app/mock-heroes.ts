a6c58f436bc038a57b0a837bd4ed6b24183614db
Beatrice N <beatrice.nita98@yahoo.com>
2022-07-07 10:06:11 +0300

    Added Tour of Heroes project

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,13 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+  { id:12, name: 'Dr. Nice'},
+  { id:13, name: 'Dr. Strange'},
+  { id:14, name: 'Iron Man'},
+  { id:15, name: 'Superman'},
+  { id:16, name: 'Superwoman'},
+  { id:17, name: 'Catwoman'},
+  { id:18, name: 'Batman'},
+  { id:19, name: 'Spiderman'},
+  { id:20, name: 'Thor'}
+];
